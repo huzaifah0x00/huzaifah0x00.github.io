4c056d8ae23f833d937b1b3cff684c3b48094bcc
huzaifah0x00 <huzaifah.asif.b@gmail.com>
2022-02-06 17:42:11 +0500

    Hi. I'm Huzaifah.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import "animate.css";
+
+export default function App() {
+  return (
+    <main>
+      <div className="about">
+        <h1 className="animate__animated animate__fadeInUp">
+          <b>Hi.</b>
+        </h1>
+        <h1 className="animate__animated animate__fadeInUp animate__delay-1s">
+          <span>I'm Huzaifah</span>
+          <span className="animate__animated animate__fadeInRight animate__delay-2s">
+            .
+          </span>
+        </h1>
+      </div>
+    </main>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Home from "./Home";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
