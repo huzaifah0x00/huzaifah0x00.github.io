eee7ac20b93a6b270a41784429766faf342f1655
huzaifah0x00 <huzaifah.asif.b@gmail.com>
2022-02-06 17:58:46 +0500

    Thanks for dropping by.

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,15 @@ import "animate.css";
 export default function App() {
   return (
     <main>
-      <div className="about">
-        <h1 className="animate__animated animate__fadeInUp">
+      <div className="about animate__animated animate__fadeIn">
+        <h1 className="animate__animated animate__fadeInUp less-margin">
           <b>Hi.</b>
         </h1>
-        <h1 className="animate__animated animate__fadeInUp animate__delay-1s">
+        <h1 className="animate__animated animate__fadeInUp animate__delay-1s less-margin">
           <span>I'm Huzaifah</span>
-          <span className="animate__animated animate__fadeInRight animate__delay-2s">
-            .
-          </span>
+          <span className="animate__animated animate__fadeInRight animate__delay-2s">.</span>
         </h1>
+        <span className="animate__animated animate__fadeIn animate__delay-2s">Thanks for dropping by</span>
       </div>
     </main>
   );
